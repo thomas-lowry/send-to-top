fc8c75d7a16fd1228d274f61072804d5256c916a
Tom Lowry <38481365+thomas-lowry@users.noreply.github.com>
2019-08-06 11:43:24 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,29 @@
+//Variables
+const selection = figma.currentPage.selection;
+const page = figma.currentPage;
+if (selection.length > 0) {
+    selection.forEach(item => {
+        //check if parent is page node
+        // if true, move to top of layer stack
+        if (item.parent.type === 'PAGE') {
+            page.appendChild(item);
+        }
+        else {
+            let parent = item.parent;
+            let xCoor = 0;
+            let yCoor = 0;
+            while (parent.type != 'PAGE') {
+                xCoor += parent.x;
+                yCoor += parent.y;
+                parent = parent.parent;
+            }
+            item.x = item.x + xCoor;
+            item.y = item.y + yCoor;
+            page.appendChild(item);
+        }
+    });
+    figma.closePlugin();
+}
+else {
+    figma.closePlugin('Please make a selection.');
+}
